fe46a1a3cc7c71b12c68958ce72520d3449a746a
Divas-Sagta <divassagta19@gmail.com>
2024-02-22 12:31:52 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects';
+import SingIn from './pages/SingIn'
+import SingUp from './pages/SingUp'
+
+
 
 export default function App() {
   return (
-    <div>
-       <h1 className='text-3xl text-red-500'>Header</h1>
-    </div>
+      <BrowserRouter>
+        <Routes>
+           <Route path="/" element={<Home />}/> 
+           <Route path="/about" element={<About />}/> 
+           <Route path="/sing-in" element={<SingIn />}/> 
+           <Route path="/sing-up" element={<SingUp />}/> 
+           <Route path="/dashboard" element={<Dashboard />}/> 
+           <Route path="/projects" element={<Projects />}/> 
+        </Routes>
+      </BrowserRouter>
   )
 }
